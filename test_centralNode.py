2a4ec64268decc1c2ac2af42459f28a2c4c2dafc
unknown <no295d@A5439594.se.nos.boeing.com>
2016-02-21 21:49:37 -0500

    updates to central node and tests

---
+++ test_centralNode.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+from CentralNode import CentralNode
+import socket
+__author__ = 'no295d'
+
+
+class TestCentralNode(TestCase):
+    def test__init_session(self):
+        self.fail()
+
+    def test__process_packet(self):
+        cn = CentralNode()
+
+        sock = self._setup_socket()
+
+        sock.sendto('1', ('localhost',CentralNode.PORT))
+
+
+        self.fail()
+
+    def test__central_node_server(self):
+        self.fail()
+
+    def test_update_node_multicast_forwarding_table(self):
+        self.fail()
+
+    def _setup_socket(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return sock
