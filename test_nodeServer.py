b21e1187df170b6ccd9f8974541660d4c725fd66
no295d <nicklausrhodes@gmail.com>
2016-05-01 13:10:38 -0400

    Adding test for node server

--- test_nodeServer.py
+++ test_nodeServer.py
@@ -27,17 +27,33 @@ class TestNodeServer(unittest.TestCase):
     def test__node_server_socket(self):
         self.fail()
 
+    # helper methods
     def build_mft(self):
-        mft = {'2':('2', '2'), '5':('3', '5'), '3':('5', '3')}
+        # dict is {'Overlay Node Id':{ 'Outgoing dest': (('Next hop id', 'Outgoing dest'), ... )}}
+        mft = {'2':(('2', '2'),), '5':(('3', '5'),), '3':(('5', '3'),)}
+        # table = et.Element('table')
+        # source_node = et.SubElement(table, 'source_node')
+        # source_node.attrib['broadcast_node'] = '1'
+        # for key, value in mft:
+        #     temp_node = et.SubElement(source_node, )
+        #     for next_hop, dest in value:
+        #         temp_node = et.SubElement(source_node, "destination_node")
+        #         temp_node.attrib['next_hop'] = next_hop
+        #         temp_node.text = dest
+        #
+        # xml_table = et.tostring(table)
+        # return xml_table
+
         table = et.Element('table')
         source_node = et.SubElement(table, 'source_node')
         source_node.attrib['broadcast_node'] = '1'
-        for key, value in mft:
-            temp_node = et.SubElement(source_node, )
-            for next_hop, dest in mft:
-                temp_node = et.SubElement(source_node, "destination_node")
-                temp_node.attrib['next_hop'] = next_hop
-                temp_node.text = dest
+        for key, value in mft.iteritems():
+            temp_node = et.SubElement(source_node, "routing_table_entry")
+            temp_node.attrib['incoming_destination'] = key
+            for next_hop, outgoing_dest in value:
+                temp_node2 = et.SubElement(temp_node, 'next_hop')
+                temp_node2.attrib['dest']=outgoing_dest
+                temp_node2.attrib['next_hop']=next_hop
 
         xml_table = et.tostring(table)
         return xml_table
